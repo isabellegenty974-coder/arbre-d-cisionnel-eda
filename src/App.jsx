b4cd8bf77b15c1d7a9487c41f63e7d11cfb6dc67
Genty Isabelle <isabellegenty974@gmail.com>
2026-05-23 14:33:46 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -195,7 +195,7 @@ const AuthenticatedApp = () => {
 
   if (authError) {
     if (authError.type === 'user_not_registered') {
-      return <UserNotRegisteredError />;
+      return <Register />;
     } else if (authError.type === 'auth_required') {
       navigateToLogin();
       return null;
